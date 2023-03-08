30bb203b313c96f4884d7d749bcff2cb757d22d9
nikccw <bik876.vaca@gmail.com>
2023-03-08 00:10:35 -0600

    hola

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'hole';
 const nuevoNum = 8;
 
 // Crea una variable booleana:
-const nuevoBool = true;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
